7f41557b0f915027d0eef9208bb5909d2ad4e724
Mohan Patil <mohan.patil@hyniva.com>
2023-09-21 15:52:28 +0530

    new js file changes

--- s3.js
+++ s3.js
@@ -1,34 +1,70 @@
-const parse = require('lambda-multipart-parser')
+const AWS = require('aws-sdk');
+const parseMultipart = require('parse-multipart');
 
-const uploadFile  = async (event) => {
-    const { file, fields } = await parse.parse(event);
-    console.log(file);
-    console.log(fields);
-    const tags = { filename: file.filename };
-    console.log(tags)
-    try {
-      await s3Client
-        .putObject({
-          //bucket name
-          Bucket: BUCKET_NAME,
-          Key: fields.filename || file.filename,
-          Body: file.content,
-          Tagging: queryString.encode(tags),
-        })
-        .promise();
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ description:'file create', result:'ok' }),
-      };
-    } catch (_error) {
-      // this is not ideal error handling, but good enough for the purpose of this example
-      return {
-        statusCode: 409,
-        body: JSON.stringify({ description: 'something went wrong', result: 'error' })
-      }
-    }
-  };
+const BUCKET = 'file-new-example-bucket';
 
-  module.exports ={
-    uploadFile
+const s3 = new AWS.S3();
+
+function extractFile(event) {
+  const contentType = event.headers['Content-Type'];
+  if (!contentType) {
+    throw new Error('Content-Type header is missing in the request.');
   }
+
+  const boundary = parseMultipart.getBoundary(contentType);
+  if (!boundary) {
+    throw new Error(
+      'Unable to determine the boundary from the Content-Type header.'
+    );
+  }
+
+  const parts = parseMultipart.Parse(
+    Buffer.from(event.body, 'base64'),
+    boundary
+  );
+  console.log('--------parts', parts);
+
+  if (!parts || parts.length === 0) {
+    throw new Error('No parts found in the multipart request.');
+  }
+
+  const [{ filename, data }] = parts;
+
+  if (!filename || !data) {
+    throw new Error(
+      'Invalid or missing filename or data in the multipart request.'
+    );
+  }
+
+  return {
+    filename,
+    data,
+  };
+}
+
+module.exports.handle = async (event) => {
+  try {
+    const { filename, data } = extractFile(event);
+    console.log('---------data', data);
+    await s3
+      .putObject({
+        Bucket: BUCKET,
+        Key: filename,
+        Body: data,
+      })
+      .promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        link: `https://${BUCKET}.s3.amazonaws.com/${filename}`,
+      }),
+    };
+  } catch (err) {
+    console.log('error-----', err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: err.message }),
+    };
+  }
+};
